e1c708883133d1dd3443fbec97f0d4bb7b6f5076
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-01-19 04:10:50 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- system/core/mkbootimg/mkbootimg.c
+++ system/core/mkbootimg/mkbootimg.c
@@ -33,10 +33,16 @@ static void *load_file(const char *fn, unsigned *_sz)
 
     data = 0;
     fd = open(fn, O_RDONLY);
-    if(fd < 0) return 0;
+    if(fd < 0) {
+        printf("can't open %s\n", fn);
+        return 0;
+    }
 
     sz = lseek(fd, 0, SEEK_END);
-    if(sz < 0) goto oops;
+    if(sz < 0) {
+        printf("can't lseek %s\n", fn);
+        goto oops;
+    }
 
     if(lseek(fd, 0, SEEK_SET) != 0) goto oops;
 
