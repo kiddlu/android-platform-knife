0f3d4e0617a1a9e1774b57dd05b7534b33228247
huangxiaolu <huangxiaolu@smartisan.com>
2016-01-21 12:13:33 +0800

    update
    Signed-off-by: huangxiaolu <huangxiaolu@smartisan.com>

--- prebuild/install-prebuild-arm.sh
+++ prebuild/install-prebuild-arm.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-cp -rf ../../prebuild/arm/adbd .
-cp -rf ../../prebuild/arm/busybox .
+
+THIS_PATH=`readlink -f $0 | xargs dirname`
+echo $THIS_PATH
+
+cp -rf $THIS_PATH/../../prebuild/arm/adbd .
+cp -rf $THIS_PATH/../../prebuild/arm/busybox .
 
 ln -s busybox '['
 ln -s busybox '[['
