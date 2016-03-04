6b9ad879d9b26139e8462bc8db91ecc2bd82ab2e
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-03-04 03:59:06 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- development/host/windows/make.bat
+++ development/host/windows/make.bat
@@ -1,5 +1,13 @@
-set WINDDK=C:\WinDDK\7600.16385.1\
+::pls download WDK 7.1 from https://www.microsoft.com/en-us/download/details.aspx?id=11800 and install
 
-%WINDDK%\bin\setenv.bat %WINDDK% fre x64 WIN7
-cd %~dp0\usb\api\ && build.exe -cbeEIFZ
-cd %~dp0\usb\winusb\ && build.exe -cbeEIFZ
+set WDKROOT=C:\WinDDK\7600.16385.1\
+set THIS_PATH=%~dp0
+
+call %THIS_PATH%\setenv.bat %WDKROOT% fre x64 WIN7
+::call %THIS_PATH%\setenv.bat %WDKROOT% fre x86 WIN7
+
+cd %THIS_PATH%\usb\api\
+build.exe -cbeEIFZ
+cd %THIS_PATH%\usb\winusb\
+build.exe -cbeEIFZ
+cd %THIS_PATH%
