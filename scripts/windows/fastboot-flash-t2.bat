8ed254993bd5f3b8a20e2bb0750257453afdb227
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-03-28 08:24:24 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- scripts/windows/fastboot-flash-t2.bat
+++ scripts/windows/fastboot-flash-t2.bat
@@ -5,10 +5,13 @@ set ADB_EXE=%DIR_PATH%\apps\adb.exe
 set FASTBOOT_EXE=%DIR_PATH%\apps\fastboot.exe
 set IMG2SIMG_EXE=%DIR_PATH%\apps\img2simg.exe
 set PACKSPARSEIMG_EXE=%DIR_PATH%\apps\packsparseimg.exe
+set BUSYBOX_EXE=%DIR_PATH%\apps\busybox.exe
 
 set FLASH_FULL=0
 set FLASH_USERDATA=0
+set FLASH_MASTERCLEAN=0
 
+%ADB_EXE% wait-for-device
 %ADB_EXE% reboot bootloader
 
 rem AP
@@ -68,15 +71,27 @@ if %FLASH_USERDATA% == 1 (
 rem reboot
 %FASTBOOT_EXE% reboot
 
-rem do master clean if not flash userdata partition
-if not %FLASH_USERDATA% == 1 (
-	%ADB_EXE% wait-for-device
-	%BUSYBOX_EXE% sleep 20
-	%ADB_EXE% shell am broadcast -a android.intent.action.MASTER_CLEAR
-)
+rem maybe should do master clean if not flash userdata partition
+if %FLASH_USERDATA% == 1 goto END
+if %FLASH_MASTERCLEAN% == 0 goto END
 
+%ADB_EXE% wait-for-device
+%BUSYBOX_EXE% sleep 5
+:LOOP
+set BOOTFLAG=0
+%ADB_EXE% shell getprop sys.boot_completed > "%TMP%\boot_completed"
+set /p BOOTFLAG=<"%TMP%\boot_completed"
+if %BOOTFLAG% == 0 (
+	echo waiting...
+	%BUSYBOX_EXE% sleep 5
+	goto LOOP
+)
+%ADB_EXE% shell am broadcast -a android.intent.action.MASTER_CLEAR
+
+:END
 echo ###################################
 echo ###### Fastboot Flash Done!! ######
 echo ###################################
+echo.
 
 pause
--- scripts/windows/fastboot-flash-t3.bat
+++ scripts/windows/fastboot-flash-t3.bat
@@ -9,7 +9,9 @@ set BUSYBOX_EXE=%DIR_PATH%\apps\busybox.exe
 
 set FLASH_FULL=0
 set FLASH_USERDATA=0
+set FLASH_MASTERCLEAN=0
 
+%ADB_EXE% wait-for-device
 %ADB_EXE% reboot bootloader
 
 rem AP
@@ -54,8 +56,8 @@ rem OEM
 %FASTBOOT_EXE% flash devcfgbak devcfg.mbn
 %FASTBOOT_EXE% flash cdt cdt.bin
 
-rem full flash
-if %FLASH_PERSIST% == 1 (
+rem Full Flash
+if %FLASH_FULL% == 1 (
 	%FASTBOOT_EXE% flash persist persist.img
 )
 if %FLASH_USERDATA% == 1 (
@@ -68,15 +70,27 @@ if %FLASH_USERDATA% == 1 (
 rem reboot
 %FASTBOOT_EXE% reboot
 
-rem do master clean if not flash userdata partition
-if not %FLASH_USERDATA% == 1 (
-	%ADB_EXE% wait-for-device
-	%BUSYBOX_EXE% sleep 20
-	%ADB_EXE% shell am broadcast -a android.intent.action.MASTER_CLEAR
-)
+rem maybe should do master clean if not flash userdata partition
+if %FLASH_USERDATA% == 1 goto END
+if %FLASH_MASTERCLEAN% == 0 goto END
 
+%ADB_EXE% wait-for-device
+%BUSYBOX_EXE% sleep 5
+:LOOP
+set BOOTFLAG=0
+%ADB_EXE% shell getprop sys.boot_completed > "%TMP%\boot_completed"
+set /p BOOTFLAG=<"%TMP%\boot_completed"
+if %BOOTFLAG% == 0 (
+	echo waiting...
+	%BUSYBOX_EXE% sleep 5
+	goto LOOP
+)
+%ADB_EXE% shell am broadcast -a android.intent.action.MASTER_CLEAR
+
+:END
 echo ###################################
 echo ###### Fastboot Flash Done!! ######
 echo ###################################
+echo.
 
 pause
